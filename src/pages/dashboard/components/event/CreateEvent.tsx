38fb0de1cc0efdd7ba7324ff992937c6ac75218e
Yume <96466759+YumeT023@users.noreply.github.com>
2022-09-17 09:32:41 +0300

    fix: form validation

--- src/pages/dashboard/components/event/CreateEvent.tsx
+++ src/pages/dashboard/components/event/CreateEvent.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
-import { FormControl, FormLabel, InputGroup, InputLeftElement, Input, FormHelperText, Select, MenuItem, SelectField, InputRightElement, InputLeftAddon, Button, Divider, HStack } from '@chakra-ui/react';
+import { FormControl, FormLabel, InputGroup, Input, FormHelperText, Select, InputRightElement, Button, Divider, HStack, Alert, AlertIcon, AlertTitle } from '@chakra-ui/react';
 import { useForm } from 'react-hook-form';
 import { Paper } from '../../../../components';
-import { FiVoicemail } from 'react-icons/fi';
-import { FaHourglass, FaHourglassEnd, FaHourglassStart, FaKey, FaLocationArrow, FaMitten, FaPersonBooth, FaTimes, FaUser } from 'react-icons/fa';
+import { FaHourglassEnd, FaHourglassStart, FaKey, FaLocationArrow, FaUser } from 'react-icons/fa';
 import { HEI_COLOR } from '../../../../utils/constant';
+import { useState } from 'react';
 
 export const CreateEvent = () => {
-  const { register, handleSubmit } = useForm();
+  const [loading, setLoading] = useState(false);
+  const { register, handleSubmit, formState: { errors, isValid } } = useForm({
+    mode: 'onChange'
+  });
+
+  // TODO: use the data-provider for this
+  const onSubmit = (data: any) => {
+    setLoading(true);
+    setTimeout(() => setLoading(false), 4000);
+  }
 
   return (
     <Paper bg={'#f7f7f7'}>
@@ -30,8 +38,14 @@ export const CreateEvent = () => {
             pointerEvents="none"
             children={<FaUser color="gray.800" />}
           />
-          <Input type="text" size="md" disabled value={'Your name'} />
+          <Input type="text" defaultValue={''} size="md" {...register('responsible', { required: 'Entrer le nom du responsable' })} isInvalid={errors.responsible && true} />
         </InputGroup>
+
+        <FormHelperText>
+          {errors.responsible ? (
+            `${errors.responsible.message}`
+          ) : 'hint: The creator of the event will be it\'s reponsible'}
+        </FormHelperText>
       </FormControl>
 
       <FormControl my={5}>
@@ -41,7 +55,7 @@ export const CreateEvent = () => {
             pointerEvents="none"
             children={<FaUser color="gray.800" />}
           />
-          <Select>
+          <Select {...register('type', { required: 'Entrer un type' })}>
             <option value="Course">Course</option>
             <option value="Event">Event</option>
           </Select>
@@ -55,7 +69,7 @@ export const CreateEvent = () => {
             pointerEvents="none"
             children={<FaLocationArrow color="gray.800" />}
           />
-          <Select>
+          <Select {...register('place', { required: 'Entrer le lieu de l\'évènement' })}>
             <option value="Course">HEI Ivandry</option>
             <option value="Event">AF Andavamamba</option>
           </Select>
@@ -69,7 +83,7 @@ export const CreateEvent = () => {
             pointerEvents="none"
             children={<FaHourglassStart color="gray.800" />}
           />
-          <Input type="datetime-local" size="md" />
+          <Input type="datetime-local" size="md" {...register('startDateTime', { required: 'cette champ est obligatoire' })} />
         </InputGroup>
       </FormControl>
 
@@ -81,16 +95,23 @@ export const CreateEvent = () => {
             pointerEvents="none"
             children={<FaHourglassEnd color="gray.800" />}
           />
-          <Input type="datetime-local" size="md" />
+          <Input type="datetime-local" size="md" {...register('endDateTime', { required: 'cette champ est obligatoire' })} />
         </InputGroup>
       </FormControl>
 
       <Divider />
 
       <HStack py={3} color="#fff">
-        <Button bg={HEI_COLOR.YELLOW} _hover={{ bg: HEI_COLOR.YELLOW_HOVER }}>Enregistrer</Button>
+        <Button
+          bg={HEI_COLOR.YELLOW}
+          _hover={{ bg: HEI_COLOR.YELLOW_HOVER }}
+          type="submit"
+          disabled={!isValid}
+          onClick={handleSubmit(onSubmit)}
+          isLoading={loading}
+        >Enregistrer</Button>
         <Button bg="red.500">Annuler</Button>
       </HStack>
-    </Paper >
+    </Paper>
   );
 }
